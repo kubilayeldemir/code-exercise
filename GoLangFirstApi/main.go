4e978b47ffda9457bb06bbccba39eed658ba0cad
kubilayeldemir <kubi0707@gmail.com>
2021-11-20 18:38:55 +0300

    Go: Book api post Book

--- GoLangFirstApi/main.go
+++ GoLangFirstApi/main.go
@@ -3,42 +3,86 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"sync"
 )
 
 func main() {
 	bookHandlers := newBookHandler()
-	http.HandleFunc("/books",bookHandlers.get)
-	
+	http.HandleFunc("/books", bookHandlers.books)
+
 	port := ":8080"
-	fmt.Println("Started listening on port",port)
-	err := http.ListenAndServe(port,nil)
+	fmt.Println("Started listening on port", port)
+	err := http.ListenAndServe(port, nil)
 	if err != nil {
-		panic("amanın haa")		
+		panic("Something wrong")
 	}
 }
 
-type  bookHandlers struct {
+type bookHandlers struct {
+	sync.Mutex
 	library map[string]Book
 }
 
-func (b *bookHandlers) get(w http.ResponseWriter, r *http.Request) {
-	var books []Book
-	for _,book := range b.library {
-		books = append(books, book)
+func (b *bookHandlers) books(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		b.get(w, r)
+	case "POST":
+		b.post(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
+}
+
+func (b *bookHandlers) get(w http.ResponseWriter, r *http.Request) {
+	books := b.getAllBooks()
 	jsonBytes, err := json.Marshal(books)
+
 	if err != nil {
-		//TODO		
-	}	
-	
+		//TODO
+	}
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (b *bookHandlers) getAllBooks() []Book {
+	b.Lock()
+	var librarySize = len(b.library)
+	var books = make([]Book, librarySize)
+	i := 0
+	for _, v := range b.library {
+		books[i] = v
+		i++
+	}
+	b.Unlock()
+	return books
+}
+
+func (b *bookHandlers) post(w http.ResponseWriter, r *http.Request) {
+	b.Lock()
+	bodyBytes, _ := ioutil.ReadAll(r.Body)
+	var newBook = Book{}
+	err := json.Unmarshal(bodyBytes, &newBook)
+	if err != nil {
+		panic("Error")
+	}
+	b.library[newBook.Name] = newBook
+	b.Unlock()
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	allBooks := b.getAllBooks()
+	jsonBytes, _ := json.Marshal(allBooks)
 	w.Write(jsonBytes)
 }
 
 func newBookHandler() *bookHandlers {
-	return  &bookHandlers{
+	return &bookHandlers{
 		library: map[string]Book{
-			"id1":{
+			"id1": {
 				Name:   "Foo Book",
 				Author: "Mr. Foo",
 				Pages:  33,
@@ -55,9 +99,8 @@ func newBookHandler() *bookHandlers {
 }
 
 type Book struct {
-	Name string
+	Name   string
 	Author string
-	Pages int
-	Price float64
+	Pages  int
+	Price  float64
 }
-
